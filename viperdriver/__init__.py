1db4fa564f007dc79aedd8a9699aaa1db84db0aa
Viper Vit <vitolg1@gmail.com>
2019-07-28 22:09:06 -0400

    Default logger level set to ERROR

--- viperdriver/__init__.py
+++ viperdriver/__init__.py
@@ -2,10 +2,10 @@ import logging
 import os
 
 console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
+console.setLevel(logging.ERROR)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 
 logger.addHandler(console)
 
