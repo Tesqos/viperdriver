a7264afbf13ec57c1d945159ea5f40036bf86707
Viper Vit <vitolg1@gmail.com>
2019-08-01 13:56:43 -0400

    tmp

--- viperdriver/__init__.py
+++ viperdriver/__init__.py
@@ -1,17 +1,11 @@
 import logging
 import os
 
-console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
-
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-
-logger.addHandler(console)
+# logger.setLevel(logging.INFO)
 
 PATH_TMP = __path__[0] + os.sep + 'tmp'
 
-# NOTE: do NOT place the import statements below before PATH_TMP
-
+# NOTE: do NOT place the following import statements above PATH_TMP
 from .src.core import SessionDriver as SessionDriver
 from .src.website import Websession as Websession
--- viperdriver/src/__init__.py
+++ viperdriver/src/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 CREDS_TYPE_PLAIN = 'json'
 CREDS_TYPE_SECURE = 'keyring'
 CREDS_TYPE_SECURE_KEY_DEFAULT = 'viperdriver'
@@ -5,3 +7,5 @@ CREDS_TYPE_SECURE_KEY_DEFAULT = 'viperdriver'
 CREDS_F_NAME = 'login'
 
 PAGES_F_NAME ='pages'
+
+logger = logging.getLogger(__name__).addHandler(logging.NullHandler())
