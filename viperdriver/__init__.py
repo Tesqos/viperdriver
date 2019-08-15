b16d492ddff11f019855b492c0f1a791fae1bfad
Viper Vit <vitolg1@gmail.com>
2019-08-15 11:21:11 -0400

    Update viperdriver/__init__.py

--- viperdriver/__init__.py
+++ viperdriver/__init__.py
@@ -4,7 +4,3 @@ import os
 logger = logging.getLogger(__name__)
 
 PATH_TMP = __path__[0] + os.sep + 'tmp' # default location of the session file
-
-# NOTE: do NOT place the following import statements above PATH_TMP
-from viperdriver.src.core import SessionDriver as SessionDriver
-from viperdriver.src.website import Websession as Websession
